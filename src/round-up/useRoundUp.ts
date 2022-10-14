6790c6c0301f523ca2cf6149e469044c44a840a8
Juntao Qiu <juntao.qiu@dominos.com.au>
2022-10-15 10:34:00 +1100

    refactor round up function a bit

--- src/round-up/useRoundUp.ts
+++ src/round-up/useRoundUp.ts
@@ -1,17 +1,20 @@
 import {useEffect, useState} from "react";
 
+function getRoundUpTotal(amount: number) {
+  return Math.floor(amount + 1);
+}
+
+function calculateTip(amount: number) {
+  return parseFloat((getRoundUpTotal(amount) - amount).toPrecision(10));
+}
+
 export const useRoundUp = (amount: number, agreeToDonate: boolean) => {
-  const [total, setTotal] = useState(amount);
+  const [total, setTotal] = useState<number>(amount);
   const [tip, setTip] = useState<number>(0);
 
   useEffect(() => {
-    const total = Math.floor(amount + 1);
-    if (agreeToDonate) {
-      setTotal(total);
-    } else {
-      setTotal(amount);
-    }
-    setTip(parseFloat((total - amount).toPrecision(10)))
+    setTotal(agreeToDonate ? getRoundUpTotal(amount) : amount);
+    setTip(calculateTip(amount));
   }, [agreeToDonate]);
 
   return {
