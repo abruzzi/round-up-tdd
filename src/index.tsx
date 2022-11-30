3a371562388e9faf806cd39c03e0e69edc5c5350
Juntao Qiu <juntao.qiu@gmail.com>
2022-12-01 09:02:11 +1100

    simplify jquery logic

--- src/index.tsx
+++ src/index.tsx
@@ -27,6 +27,8 @@ const renderPaymentMethods = (paymentMethods: PaymentMethod[]) => {
   );
 };
 
+
+
 const renderPaymentButton = (strategy: PaymentStrategy, total: number) => {
   return $("<button>", { class: "paymentButton" }).text(
     formatButtonLabel(strategy, total)
@@ -66,17 +68,28 @@ $(() => {
     "https://5a2f495fa871f00012678d70.mockapi.io/api/payment-methods?countryCode=AU";
   const client = new FetchClient(url);
 
+  client.fetch().then((methods) => {
+    const paymentMethods = renderPaymentMethods(methods);
+    paymentMethods.appendTo(container);
+  });
+});
+
+$(() => {
+  const container = $("#root").find(".container");
+
+  const url =
+    "https://5a2f495fa871f00012678d70.mockapi.io/api/payment-methods?countryCode=AU";
+  const client = new FetchClient(url);
+
   const strategy = new PaymentStrategyDK();
   const amount = 13.8;
   const tip = strategy.getTip(amount);
 
-  client.fetch().then((paymentMethods) => {
-    const $paymentMethods = renderPaymentMethods(paymentMethods);
-    $paymentMethods.appendTo(container);
-
-    const $button = renderPaymentButton(strategy, amount);
-
+  client.fetch().then((methods) => {
     let agreeToDonate = false;
+
+    const paymentMethods = renderPaymentMethods(methods);
+
     const onChange = () => {
       agreeToDonate = !agreeToDonate;
       const total = agreeToDonate ? strategy.getRoundUpAmount(amount) : amount;
@@ -87,13 +100,15 @@ $(() => {
       );
     };
 
-    const $checkbox = renderCheckbox(
+    const checkbox = renderCheckbox(
       onChange,
       agreeToDonate,
       formatCheckboxLabel(agreeToDonate, tip, strategy)
     );
+    const button = renderPaymentButton(strategy, amount);
 
-    $checkbox.appendTo(container);
-    $button.appendTo(container);
+    paymentMethods.appendTo(container);
+    checkbox.appendTo(container);
+    button.appendTo(container);
   });
 });
