b944494e2df0f7a70cdbda10d13f46142f50d970
Juntao Qiu <juntao.qiu@gmail.com>
2022-12-05 15:20:27 +1100

    added tenstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import {OrderDetails} from './order/OrderDetails';
 import {Payment} from "./round-up/Payment";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className="app">
-      <OrderDetails />
-      <Payment amount={19.8} />
+      <QueryClientProvider client={queryClient}>
+        <OrderDetails />
+        <Payment amount={19.8} />
+      </QueryClientProvider>
     </div>
   );
 }
--- src/round-up/usePaymentMethods.ts
+++ src/round-up/usePaymentMethods.ts
@@ -1,24 +1,24 @@
-import { useEffect, useState } from "react";
-import { PaymentMethod } from "./PaymentMethod";
 import { FetchClient } from "./FetchClient";
+
+import { useQuery } from "@tanstack/react-query";
 
 const url =
   "https://5a2f495fa871f00012678d70.mockapi.io/api/payment-methods?countryCode=AU";
 const client = new FetchClient(url);
 
 export const usePaymentMethods = () => {
-  const [paymentMethods, setPaymentMethods] = useState<PaymentMethod[]>([]);
-
-  useEffect(() => {
-    const fetchPaymentMethods = async () => {
-      const methods = await client.fetch();
-      setPaymentMethods(methods)
-    };
-
-    fetchPaymentMethods();
+  const {
+    data: paymentMethods = [],
+    error,
+    status,
+  } = useQuery({
+    queryKey: ["paymentMethods"],
+    queryFn: () => client.fetch(),
   });
 
   return {
-    paymentMethods
-  }
-}
+    paymentMethods,
+    error,
+    status,
+  };
+};
